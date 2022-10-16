72b3ef9fe582ca35dbd084d9b58a5006e1163edc
Sanaz <szeraati69@gmail.com>
2022-10-16 23:14:56 +0330

    create-react-app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//React 18
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
